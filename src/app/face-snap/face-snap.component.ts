53fcc0cc0faeb6e2fefe5781968ddfd8abcf89b5
steve-237 <steve0peg@gmail.com>
2024-09-15 22:26:45 +0200

    initialise project and add basic component

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,45 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FaceSnap } from '../models/face-snap';
+import { NgClass, NgStyle, TitleCasePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-face-snap',
+  standalone: true,
+  imports: [
+    NgStyle,
+    NgClass,
+    TitleCasePipe
+  ],
+  templateUrl: './face-snap.component.html',
+  styleUrl: './face-snap.component.scss'
+})
+export class FaceSnapComponent implements OnInit{
+  @Input() faceSnap!: FaceSnap;
+
+  userHasSnapped: boolean = false;
+  snapButtonText!: string;
+
+  ngOnInit(): void {
+      this.snapButtonText = 'Oh Snap!'
+    }
+
+  onSnap(): void {
+    if(this.userHasSnapped) {
+      this.unSanp();
+    } else {
+      this.snap();
+    }
+  }
+
+  unSanp(): void {
+    this.faceSnap.removeSnap();
+    this.snapButtonText = 'Oh Snap!';
+    this.userHasSnapped = false;
+  }
+
+  snap(): void {
+    this.faceSnap.addSnap();
+    this.snapButtonText = 'Ooops, Unsnap!';
+    this.userHasSnapped = true;
+  }
+}
